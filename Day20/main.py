07ef0796bfbe72892c2cf5527c00eaffac81fc1c
sypai <7suyashbajpai@gmail.com>
2022-11-28 16:05:07 +0530

    Completed Day20. Built Snake Game (part 1). The snake movements bound to key presses

--- Day20/main.py
+++ Day20/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 # Setting the screen up
@@ -7,50 +8,24 @@ screen.setup(width=700, height=700)
 screen.bgcolor("#222222")
 screen.title("Snake Game")
 screen.tracer(0)
-screen.listen()
 
-# Creating the snake body
-cells = 2
-curr_x = 0
-curr_y = 0
 
-snake = []
+s = Snake()
 
-while cells > 0:
-    cell = Turtle(shape="square")
-    # t.resizemode("user")
-    # t.shapesize(stretch_wid=1, stretch_len=1)
-    cell.color("#eeeeee")
-    cell.pu()
-    cell.setpos(curr_x, curr_y)
-    snake.append(cell)
-    cells -= 1
-    curr_x -= 20
 
-# Snake Movement on user input
-user_direction = None # up, down, right, left
-
-# Moving the Snake
+# Game Engine
 game_is_on = True
-size = len(snake)
-
 while game_is_on:
-    
     screen.update()
-
-    # Moving the body of the snake forward
-    for idx in range(size - 1, 0, -1):
-        curr_cell = snake[idx]
-        next_cell = snake[idx-1]
-        curr_cell.goto(next_cell.xcor(), next_cell.ycor())
-    
-    # Moving the head of the snake forward (or Turning on user input)
-
-    snake[0].fd(20)
-
-    
     time.sleep(0.1)
-    
+    s.move()
+
+    # Snake Movement on user input
+    screen.listen()
+    screen.onkey(fun=s.up, key="Up")
+    screen.onkey(fun=s.down, key="Down")    
+    screen.onkey(fun=s.left, key="Left")
+    screen.onkey(fun=s.right, key="Right")
 
 
 screen.exitonclick()
--- Day20/snake.py
+++ Day20/snake.py
@@ -1,10 +1,17 @@
 from turtle import Turtle
 
+MOVE_DISTANCE=20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     def __init__(self) -> None:
         self.snake = []
         self.size = 0
         self.manifest()
+        self.head = self.snake[0]
 
     def manifest(self):
         cell = Turtle(shape="square")
@@ -20,5 +27,25 @@ class Snake:
             next_cell = self.snake[idx-1]
             curr_cell.goto(next_cell.xcor(), next_cell.ycor())
         
-        # Moving the head of the snake forward (or Turning on user input)
-        self.snake[0].fd(20)
+        # Moving the head of the snake forward
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() == 270:
+            return
+        self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() == 90:
+            return
+        self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() == 0:
+            return
+        self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() == 180:
+            return
+        self.head.setheading(RIGHT)
